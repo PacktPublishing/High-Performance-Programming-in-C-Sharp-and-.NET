960798534cd3b9ea5fb6629b4379537ee40d8943
Jason Alls <jasonalls@hotmail.com>
2022-05-30 04:17:51 +0100

    Added CH15 code.

---
+++ CH15/CH15_ParallelProgramming/Benchmarks.cs
@@ -0,0 +1,144 @@
+﻿namespace CH15_ParallelProgramming
+{
+    using BenchmarkDotNet.Attributes;
+    using Nessos.LinqOptimizer.CSharp;
+
+    public class Benchmarks
+    {
+        private short[] data;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = new short[short.MaxValue];
+            for (short x = 1; x <= data.Length - 1; x++)
+            {
+                data[x] = x;
+            }
+        }
+
+        [Benchmark]
+        public void StandardForEachLoopExample()
+        {
+            foreach (int x in data)
+                Console.WriteLine($"Item {x}: {x}");
+        }
+
+
+        [Benchmark]
+        public void ParallelForEachLoopExample()
+        {
+            Parallel.ForEach(data, x => {
+                Console.WriteLine($"Item {x}: {x}");
+            });
+        }
+
+        [Benchmark]
+        public List<string> UrlDownloader1()
+        {
+            List<string> urlContent = new();
+            HttpClient client = new();
+
+            var urls = new[]
+                {
+                    "http://google.com",
+                    "http://yahoo.com",
+                    "http://stackoverflow.com"
+                };
+
+            foreach (var url in urls)
+            {
+                Console.WriteLine($"starting to download {url}");
+                string result = client.GetStringAsync(url).GetAwaiter().GetResult();
+                urlContent.Add(result);
+                Console.WriteLine($"finished downloading {url}");
+            }
+
+            client.Dispose();
+
+            return urlContent;
+        }
+
+        [Benchmark]
+        public List<string> UrlDownloader2()
+        {
+            List<string> urlContent = new();
+
+            var urls = new[]
+                {
+                    "http://google.com",
+                    "http://yahoo.com",
+                    "http://stackoverflow.com"
+                };
+
+            var tasks = urls
+                .Select(url => Task.Factory.StartNew(
+                    state =>
+                    {
+                        using var client = new HttpClient();
+                        var url = (string)state;
+                        Console.WriteLine($"starting to download {url}");
+                        string result = client.GetStringAsync(url).GetAwaiter().GetResult();
+                        urlContent.Add(result);
+                        Console.WriteLine($"finished downloading {url}");
+                    }, url)
+                )
+                .ToArray();
+
+            Task.WaitAll(tasks);
+            return urlContent;
+        }
+
+        [Benchmark]
+        public List<string> UrlDownloader3()
+        {
+            List<string> urlContent = new();
+            HttpClient client = new();
+
+            List<string> urls = new()
+                {
+                    "http://google.com",
+                    "http://yahoo.com",
+                    "http://stackoverflow.com"
+                };
+
+            urls.ForEach(url =>
+            {
+                Console.WriteLine($"starting to download {url}");
+                string result = client.GetStringAsync(url).GetAwaiter().GetResult();
+                urlContent.Add(result);
+                Console.WriteLine($"finished downloading {url}");
+            });
+
+            client.Dispose();
+
+            return urlContent;
+        }
+
+        [Benchmark]
+        public List<string> UrlDownloader4()
+        {
+            List<string> urlContent = new();
+            HttpClient client = new();
+
+            List<string> urls = new()
+            {
+                "http://google.com",
+                "http://yahoo.com",
+                "http://stackoverflow.com"
+            };
+
+            Parallel.ForEach(urls, url =>
+            {
+                Console.WriteLine($"starting to download {url}");
+                string result = client.GetStringAsync(url).GetAwaiter().GetResult();
+                urlContent.Add(result);
+                Console.WriteLine($"finished downloading {url}");
+            });
+
+            client.Dispose();
+
+            return urlContent;
+        }
+    }
+}
---
+++ CH15/CH15_ParallelProgramming/Program.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace CH15_ParallelProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] _)
+        {
+            //RunSingleProcessorExample();
+            //Parallel.For(0, 1000000, x => MultipleProcessorExample(x));
+            BenchmarkRunner.Run<Benchmarks>();
+        }
+
+        static void RunSingleProcessorExample()
+        {
+            Thread thread = new(SingleProcessorExample);
+            thread.Start();
+        }
+
+        static void SingleProcessorExample()
+        {
+            string output = "Index: ";
+
+            for (int index = 0; index < 1000000; index++)
+            {
+                Console.WriteLine($"{output}{index}");
+            }
+            
+            Console.ReadKey();
+        }
+
+        static void MultipleProcessorExample(int index)
+        {
+            string output = "Index: ";
+            Console.WriteLine($"{output}{index}");
+        }
+    }
+}
