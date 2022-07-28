0ca406d94739655c4118ccceb21ebf4431ac6c07
Jason Alls <jasonalls@hotmail.com>
2022-07-28 22:25:19 +0100

    Fixed GRPC Client/Server application.

--- CH09/CH09_GrpcServiceClient/Program.cs
+++ CH09/CH09_GrpcServiceClient/Program.cs
@@ -1,48 +1,47 @@
-﻿using CH09_GrpcService;
+﻿namespace CH09_GrpcServiceClient;
+
+using CH09_GrpcService;
 using Grpc.Core;
 using Grpc.Net.Client;
 using System;
 using System.Threading.Tasks;
 
-namespace CH09_GrpcServiceClient
+class Program
 {
-	class Program
+	static async Task Main(string[] args)
 	{
-		static async void Main(string[] args)
-		{
-			await ExecuteGrpcClient();
-		}
+		await ExecuteGrpcClient();
+	}
 
-		static async Task ExecuteGrpcClient()
-		{
-			await SingleGrpcMessageResponse();
-			await GrpcMessageResponseStream();
-		}
+	static async Task ExecuteGrpcClient()
+	{
+		await SingleGrpcMessageResponse();
+		await GrpcMessageResponseStream();
+	}
 
-		static async Task SingleGrpcMessageResponse()
+	static async Task SingleGrpcMessageResponse()
+	{
+		GrpcChannel grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+		Greeter.GreeterClient greeterClient = new Greeter.GreeterClient(grpcChannel);
+		HelloReply helloReply = await greeterClient.SayHelloAsync(new HelloRequest
 		{
-			GrpcChannel grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
-			Greeter.GreeterClient greeterClient = new Greeter.GreeterClient(grpcChannel);
-			HelloReply helloReply = await greeterClient.SayHelloAsync(new HelloRequest
-			{
-				Name = "gRPC Demonstration!"
-			});
-			Console.WriteLine($"Message From gRPC Server: {helloReply.Message}");
-		}
+			Name = "gRPC Demonstration!"
+		});
+		Console.WriteLine($"Message From gRPC Server: {helloReply.Message}");
+	}
 
-		static async Task GrpcMessageResponseStream()
+	static async Task GrpcMessageResponseStream()
+	{
+		GrpcChannel grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+		Greeter.GreeterClient greeterClient = new Greeter.GreeterClient(grpcChannel);
+		AsyncServerStreamingCall<HelloReply> helloReply = greeterClient.SayHelloStream(new HelloRequest
 		{
-			GrpcChannel grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
-			Greeter.GreeterClient greeterClient = new Greeter.GreeterClient(grpcChannel);
-			AsyncServerStreamingCall<HelloReply> helloReply = greeterClient.SayHelloStream(new HelloRequest
-			{
-				Name = "gRPC Streaming Demonstration!"
-			});
-			
-			await foreach (HelloReply item in helloReply.ResponseStream.ReadAllAsync())
-			{
-				Console.WriteLine(item.Message);
-			}
+			Name = "gRPC Streaming Demonstration!"
+		});
+		
+		await foreach (HelloReply item in helloReply.ResponseStream.ReadAllAsync())
+		{
+			Console.WriteLine(item.Message);
 		}
 	}
 }
